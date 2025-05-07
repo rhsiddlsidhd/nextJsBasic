dd9ec5c5b4519f3b732fc590375b02dddbe67e19
Young <rhsiddlsidhd@nate.com>
2025-05-08 08:32:31 +0900

    next/Link

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const page = async () => {
   const message = `${new Date().toLocaleString()}의 SSR`;
 
-  return <p>{message}</p>;
+  return (
+    <div>
+      <p>{message}</p>
+      <Link
+        href={{
+          pathname: "/ssg",
+        }}
+      >
+        <button>Go to SSG</button>
+      </Link>
+    </div>
+  );
 };
 
 export default page;
